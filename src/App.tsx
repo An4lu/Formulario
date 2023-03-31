f47685c979511bb8f2aa83d000d76d6e4393d0c7
Analu <anabluoliveira@outlook.com>
2023-03-31 16:24:23 -0300

    Melhoria nos Componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Sidebar } from './components/Sidebar'
-import { globalStyles } from './styles/global'
+import { MultiSelect } from "./components/MultiSelect";
+import { Sidebar } from "./components/Sidebar";
+import { globalStyles } from "./styles/global";
 
 export function App() {
-  globalStyles()
-  return <Sidebar />
+  globalStyles();
+  return (
+    <div>
+      <Sidebar />
+      {/* <h1>Oidkbnfkhsbfgjhsdfbgjhsdbfgjhsdbfg</h1>
+      <MultiSelect options={[{ label: "Chuva", value: "Água" }]} /> */}
+    </div>
+  );
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { createStitches } from '@stitches/react'
+import { createStitches } from "@stitches/react";
 
 export const {
   config,
@@ -12,17 +12,16 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      yellow: '#D3AB3C',
-      darkblue: '#122137',
-      gray100: '#9D9D9D',
-      gray300: '#9F9999',
-      gray400: '#797777',
-      gray500: '#2B2B2B',
-      yellow600: '#C39D43',
-      lightblue: '#5CC8FF',
-      blueHover: '#21425F',
-      red: '#EF4444',
-      white: '#FFFFFF',
+      yellow100: "#D3AB3C",
+      yellow600: "#C39D43",
+      blue50: "#5CC8FF",
+      blue100: "#21425F",
+      blue500: "#122137",
+      gray100: "#9D9D9D",
+      gray300: "#9F9999",
+      gray400: "#797777",
+      gray500: "#2B2B2B",
+      white: "#FFFFFF",
     },
   },
-})
+});
